42271ef16958087b09f73534a404036cf788e345
Anthony Rojas <anthony.rojas095@gmail.com>
2019-03-13 21:52:38 -0700

    removed movies and movie controller, seems unecessary

--- ComicsShowcase/Models/ComicsContext.cs
+++ ComicsShowcase/Models/ComicsContext.cs
@@ -21,13 +21,11 @@ namespace ComicsShowcase.Models
             modelBuilder.Entity<GraphicNovel>().Property(e => e.Publisher).HasConversion(v => v.ToString(), v => (Publisher)Enum.Parse(typeof(Publisher), v));
             modelBuilder.Entity<GraphicNovel>().Property(e => e.BookCondition).HasConversion(v => v.ToString(), v => (BookCondition)Enum.Parse(typeof(BookCondition), v));
             modelBuilder.Entity<Collectible>().Property(e => e.ItemCategory).HasConversion(v => v.ToString(), v => (CollectibleCategory)Enum.Parse(typeof(CollectibleCategory), v));
-            modelBuilder.Entity<Movie>().Property(e => e.DiskType).HasConversion(v => v.ToString(), v => (DiskType)Enum.Parse(typeof(DiskType), v));
         }
         public DbSet<User> Users { get; set; }
         public DbSet<ComicBook> Comics { get; set; }
         public DbSet<Creator> Creators { get; set; }
         public DbSet<GraphicNovel> GraphicNovels { get; set; }
         public DbSet<Collectible> Collectibles { get; set; }
-        public DbSet<Movie> Movies { get; set; }
     }
 }
